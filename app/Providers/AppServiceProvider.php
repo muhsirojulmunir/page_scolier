43ea8bccc5e3505c949540daf96f858cd03ee6f1
muhsirojulmunir <munirxanm40@gmail.com>
2026-08-26 10:55:16 +0700

    feat: automatic dynamic asset sync to public_html on app boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Otomatis sinkronkan aset build dan gambar ke public_html pada shared hosting
+        $publicHtml = base_path('../public_html');
+        if (is_dir($publicHtml)) {
+            $srcManifest = public_path('build/manifest.json');
+            $destManifest = $publicHtml . '/build/manifest.json';
+            if (file_exists($srcManifest) && (! file_exists($destManifest) || filemtime($srcManifest) > filemtime($destManifest))) {
+                self::copyDirectory(public_path('build'), $publicHtml . '/build');
+                if (is_dir(public_path('img'))) {
+                    self::copyDirectory(public_path('img'), $publicHtml . '/img');
+                }
+            }
+        }
+
         /*
          * Bagikan pembuat tautan WhatsApp ke seluruh view.
          *
@@ -33,5 +46,32 @@ class AppServiceProvider extends ServiceProvider
 
             return 'https://wa.me/' . $number . '?text=' . rawurlencode($text);
         });
+    }
+
+    /**
+     * Salin direktori secara rekursif.
+     */
+    protected static function copyDirectory(string $src, string $dest): void
+    {
+        if (! is_dir($src)) {
+            return;
+        }
+        if (! is_dir($dest)) {
+            @mkdir($dest, 0755, true);
+        }
+        $dir = @opendir($src);
+        if (! $dir) {
+            return;
+        }
+        while (($file = readdir($dir)) !== false) {
+            if ($file !== '.' && $file !== '..') {
+                if (is_dir($src . '/' . $file)) {
+                    self::copyDirectory($src . '/' . $file, $dest . '/' . $file);
+                } else {
+                    @copy($src . '/' . $file, $dest . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
     }
 }
